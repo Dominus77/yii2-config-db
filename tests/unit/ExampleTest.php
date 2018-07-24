5e5cdabcd9dd2702acf95576bc7729d10caf7017
Alexey Schevchenko <ivanovosity@gmail.com>
2018-07-25 00:29:48 +0300

    commit

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace modules\config\tests\unit;
+
+use Yii;
+
+/**
+ * Class ExampleTest
+ * @package modules\config\tests\unit
+ */
+class ExampleTest extends \Codeception\Test\Unit
+{
+    /**
+     * @inheritdoc
+     */
+    public function testExample()
+    {
+        return true;
+    }
+}
