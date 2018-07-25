4111565b011684c94ed9f86b083081ff6d9d1e35
Alexey Schevchenko <ivanovosity@gmail.com>
2018-07-25 23:57:01 +0300

    add coverage

--- tests/unit/ConfigTest.php
+++ tests/unit/ConfigTest.php
@@ -3,6 +3,7 @@
 namespace modules\config\tests\unit;
 
 use Yii;
+use yii\base\InvalidConfigException;
 use modules\config\models\Config;
 use modules\config\params\ConfigParams;
 
@@ -12,6 +13,11 @@ use modules\config\params\ConfigParams;
  */
 class ConfigTest extends \Codeception\Test\Unit
 {
+    /**
+     * @var \modules\config\tests\UnitTester
+     */
+    protected $tester;
+
     /**
      * This get default value
      */
@@ -53,9 +59,38 @@ class ConfigTest extends \Codeception\Test\Unit
     }
 
     /**
-     * This set value
+     * This get not params
      */
-    public function testSetValue()
+    public function testGetValueNotParam()
+    {
+        $this->tester->expectException(new InvalidConfigException('Undefined parameter NONE_PARAM'), function () {
+            $this->getNotParam();
+        });
+    }
+
+    /**
+     * @return InvalidConfigException
+     */
+    protected function getNotParam()
+    {
+        $model = new Config([
+            'param' => 'TEST_NAME',
+            'label' => 'Test Name',
+            'value' => '',
+            'type' => ConfigParams::FIELD_TYPE_STRING,
+            'default' => 'Tester',
+        ]);
+        $this->assertTrue($model->save());
+
+        $app = Yii::$app;
+        $config = $app->config;
+        return $config->get('NONE_PARAM');
+    }
+
+    /**
+     * This set value success
+     */
+    public function testSetValueSuccess()
     {
         $model = new Config([
             'param' => 'TEST_NAME',
