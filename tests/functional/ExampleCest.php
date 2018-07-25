13cc08d57110ffa72325bbe6e293463bff78e19c
Alexey Schevchenko <ivanovosity@gmail.com>
2018-07-25 05:23:33 +0300

    add functional test

---
+++ tests/functional/ExampleCest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace modules\config\tests\functional;
+
+use Yii;
+use modules\config\tests\FunctionalTester;
+
+/**
+ * Class ExampleCest
+ * @package modules\config\tests\functional
+ */
+class ExampleCest
+{
+    /**
+     * @param FunctionalTester $I
+     */
+    public function checkUsers(FunctionalTester $I)
+    {
+
+    }
+}
