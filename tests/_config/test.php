f633e5e34b0cb3b29d822c86db1607870f5817f2
Alexey Schevchenko <ivanovosity@gmail.com>
2018-07-25 05:35:03 +0300

    commit

--- tests/_config/test.php
+++ tests/_config/test.php
@@ -1,7 +1,7 @@
 <?php
-
+defined('YII_APP_BASE_PATH') || define('YII_APP_BASE_PATH', __DIR__ . '/../../../../');
 return yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../../../../common/config/test-local.php'),
+    require(YII_APP_BASE_PATH . '/common/config/test-local.php'),
     [
         'id' => 'module-config',
         'language' => 'en',
